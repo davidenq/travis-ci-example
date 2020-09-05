c59bd08f78a18152bba98d1d029920bdf1ac74a1
David <davip@live.com>
2020-09-04 23:35:21 -0500

    add script for deploy production environment

--- deploy_production.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-gcloud run deploy travis-ci-example --image ${CONTAINER_TAG}:${TRAVIS_COMMIT} --region us-east1 --platform managed --allow-unauthenticated
--- deploy_staging.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-gcloud run deploy ${TRAVIS_BUILD_ID}-staging --image ${CONTAINER_TAG}:${TRAVIS_COMMIT} --region us-east1 --platform managed --allow-unauthenticated
