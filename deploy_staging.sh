b86d35b849fa8b117e6fa917dce9f78f62f22e70
David <davip@live.com>
2020-09-04 22:42:37 -0500

    test wih login through bash to deploy

--- deploy_staging.sh
+++ deploy_staging.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+gcloud auth activate-service-account ${GOOGLE_ACCOUNT} --key-file=./devops-flow-testing.json
+gcloud auth configure-docker
+docker login -u _json_key --password-stdin https://gcr.io < devops-flow-testing.json
 gcloud run deploy ${TRAVIS_BUILD_ID}-staging --image ${CONTAINER_TAG}:${TRAVIS_COMMIT} --region us-east1 --platform managed --allow-unauthenticated
